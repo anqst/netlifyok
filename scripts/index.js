129f2f8fd1cf3d595ed6bc369938db540bc6e8f7
mocekadam <mocek.ad.2020@skola.ssps.cz>
2022-06-09 11:01:47 +0200

    unstyled form, nice

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const form = document.querySelector("#testing-jailbreak");
+    const math4 = form.querySelector("#math-4");
+    const anything = form.querySelector("#anything");
+
+    form.addEventListener("submit", function(e){
+
+        /* e.preventDefault();
+        console.log("form submitted"); */
+    });
+});
